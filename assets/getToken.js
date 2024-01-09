1d1c3a29ef90d9e6a919a9eb1c9463204044d843
Miguel Caparróz <miguelpicolocaparroz@gmail.com>
2024-01-08 22:05:14 -0400

    Create getToken.js

---
+++ assets/getToken.js
@@ -0,0 +1,26 @@
+const key = Object.keys(window.localStorage).filter((function(e) {
+        return e.startsWith("@@auth0spajs@@")
+    }
+))
+const token = JSON.parse(localStorage.getItem(key[0])).body.access_token
+
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  },
+  body: {}
+};
+
+fetch("https://api.openai.com/dashboard/onboarding/login", options)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Handle the response data here
+    console.log("Your session token is: " + data.user.session.sensitive_id);
+  })
